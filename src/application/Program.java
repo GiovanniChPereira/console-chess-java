aa894527165c603a8725202465ef3f7d6f32dc47
GiovanniChPereira <giovanni.ch.pereira@gmail.com>
2023-11-19 21:38:12 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,46 +1,41 @@
 package application;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
-
 public class Program {
-
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
-
-		ChessMatch chessmatch = new ChessMatch();
-
+		ChessMatch chessMatch = new ChessMatch();
+		
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessmatch.getPieces());
+				UI.printBoard(chessMatch.getPieces());
 				System.out.println();
-				System.out.println("Source: ");
+				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				System.out.println();
-				System.out.println("Target: ");
-				ChessPosition target = UI.readChessPosition(sc);
-	
-				ChessPiece capturePiece = chessmatch.performChessMove(source, target);
-			}
-			catch(ChessException e){
-				System.out.println(e.getMessage());
-				sc.nextLine();
-				
-			}
-			catch(InputMismatchException e){
-				System.out.println(e.getMessage());
-				sc.nextLine();
-				
-			}
 
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
-
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -2,15 +2,12 @@ package application;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
-
 	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
-
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -20,7 +17,6 @@ public class UI {
 	public static final String ANSI_PURPLE = "\u001B[35m";
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
-
 	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
 	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
 	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
@@ -43,7 +39,7 @@ public class UI {
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
 		} catch (RuntimeException e) {
-			throw new InputMismatchException("Error instantiating ChessPosition: Valid values are from a1 to a8");
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
 	}
 
@@ -51,16 +47,30 @@ public class UI {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
 		if (piece == null) {
-			System.out.print("-");
+			System.out.print("-" + ANSI_RESET);
 		} else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
@@ -70,5 +80,4 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-
 }
